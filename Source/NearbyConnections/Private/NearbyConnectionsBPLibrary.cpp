9a76937fde3db5de400d3549e01c226bc2c1ef4d
Taras Leskiv <leskiv.taras@gmail.com>
2021-12-26 11:37:36 +0200

    Initial commit

---
+++ Source/NearbyConnections/Private/NearbyConnectionsBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NearbyConnectionsBPLibrary.h"
+#include "NearbyConnections.h"
+
+UNearbyConnectionsBPLibrary::UNearbyConnectionsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UNearbyConnectionsBPLibrary::NearbyConnectionsSampleFunction(float Param)
+{
+	return -1;
+}
+
