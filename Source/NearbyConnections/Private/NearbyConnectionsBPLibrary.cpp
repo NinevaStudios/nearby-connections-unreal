b10d38f1de4337e7c1d3416b0a5f49013b7d517a
Andriy Marshalek <amarshalek@ninevastudios.com>
2021-12-27 13:00:09 +0200

    declare functions in c++

--- Source/NearbyConnections/Private/NearbyConnectionsBPLibrary.cpp
+++ Source/NearbyConnections/Private/NearbyConnectionsBPLibrary.cpp
@@ -2,3 +2,23 @@
 
 #include "NearbyConnectionsBPLibrary.h"
 
+FNearbyConnectionsVoidDelegate UNearbyConnectionsBPLibrary::OnStartAdvertisingSuccess;
+FNearbyConnectionsStringDelegate UNearbyConnectionsBPLibrary::OnStartAdvertisingError;
+
+FNearbyConnectionsVoidDelegate UNearbyConnectionsBPLibrary::OnStartDiscoverySuccess;
+FNearbyConnectionsStringDelegate UNearbyConnectionsBPLibrary::OnStartDiscoveryError;
+
+void UNearbyConnectionsBPLibrary::StartAdvertising(const FNearbyConnectionOptions Options, const FString& UserName, const FString& ServiceId,
+	const FNearbyConnectionsVoidDelegate& OnSuccess, const FNearbyConnectionsStringDelegate& OnError)
+{
+	OnStartAdvertisingSuccess = OnSuccess;
+	OnStartAdvertisingError = OnError;
+}
+
+void UNearbyConnectionsBPLibrary::StartDiscovery(const FNearbyConnectionOptions Options, const FString& ServiceId,
+	const FNearbyConnectionsVoidDelegate& OnSuccess, const FNearbyConnectionsStringDelegate& OnError)
+{
+	OnStartDiscoverySuccess = OnSuccess;
+	OnStartDiscoveryError = OnError;
+}
+
--- Source/NearbyConnections/Public/NearbyConnectionsBPLibrary.h
+++ Source/NearbyConnections/Public/NearbyConnectionsBPLibrary.h
@@ -5,9 +5,53 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "NearbyConnectionsBPLibrary.generated.h"
 
+UENUM()
+enum class ENearbyConnectionsStrategy : uint8
+{
+	Cluster = 0,
+	PointToPoint = 1,
+	Star = 2
+};
+
+USTRUCT(BlueprintType)
+struct FNearbyConnectionOptions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, Category = "Nearby Connections")
+	ENearbyConnectionsStrategy Strategy;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Nearby Connections")
+	bool IsLowPower;
+
+	/// <summary>
+	/// Used only for StartAdvertising, ignored in StartDiscovery.
+	/// </summary>
+	UPROPERTY(BlueprintReadOnly, Category = "Nearby Connections")
+	bool IsDisruptiveUpgrade;
+};
+
+DECLARE_DYNAMIC_DELEGATE(FNearbyConnectionsVoidDelegate);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FNearbyConnectionsStringDelegate, FString, String);
+
 UCLASS()
 class UNearbyConnectionsBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Nearby Connections", meta = (AutoCreateRefTerm = "OnSuccess,OnError"))
+	static void StartAdvertising(const FNearbyConnectionOptions Options, const FString& UserName, const FString& ServiceId,
+		const FNearbyConnectionsVoidDelegate& OnSuccess, const FNearbyConnectionsStringDelegate& OnError);
+
+	UFUNCTION(BlueprintCallable, Category = "Nearby Connections", meta = (AutoCreateRefTerm = "OnSuccess,OnError"))
+	static void StartDiscovery(const FNearbyConnectionOptions Options, const FString& ServiceId,
+		const FNearbyConnectionsVoidDelegate& OnSuccess, const FNearbyConnectionsStringDelegate& OnError);
+
+	static FNearbyConnectionsVoidDelegate OnStartAdvertisingSuccess;
+	static FNearbyConnectionsStringDelegate OnStartAdvertisingError;
+	
+	static FNearbyConnectionsVoidDelegate OnStartDiscoverySuccess;
+	static FNearbyConnectionsStringDelegate OnStartDiscoveryError;
 };
